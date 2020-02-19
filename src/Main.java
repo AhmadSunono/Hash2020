46908036757353b56c45e59cc2db5ddded99658c
Ahmad M. Sunono <ahmad.m.snono@gmail.com>
2020-02-19 20:20:26 +0200

    Adding IO methods.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+	
+	public static int potato() throws IOException{
+		
+//		BufferedReader bufferedReader = new BufferedReader(new FileReader("a_example.in"));
+//		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out1.txt"));
+//		
+//		bufferedReader.readLine();
+//		
+//		
+//		bufferedWriter.write("potato");
+//		bufferedWriter.newLine();
+//		
+//		
+//		bufferedReader.close();
+//		bufferedWriter.close();
+
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Potato!");
