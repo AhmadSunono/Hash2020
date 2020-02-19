374ef340d6672d0ce650b2165cbfa945d6691886
mohammeddwikat <43478667+mohammeddwikat@users.noreply.github.com>
2020-02-19 20:36:45 +0200

    remove every thing

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Potato!");
-		System.out.println("Potato!");
-
-		
-		
+		System.out.println("Potato!");		
 	}
 
 }
